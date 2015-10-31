d64d04a0f6884c7eebb83bf5934b7f9f94974d31
Lucifer-is-my-pet <dianka-piratka@list.ru>
2015-10-31 23:39:38 +0500

    Немношк поправок, чтоб были (без лишнего)

--- lego.js
+++ lego.js
@@ -1,15 +1,5 @@
 'use strict';
 
-var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
-var ARGUMENT_NAMES = /([^\s,]+)/g;
-function getParamNames(func) {
-    var fnStr = func.toString().replace(STRIP_COMMENTS, '');
-    var result = fnStr.slice(fnStr.indexOf('(')+1, fnStr.indexOf(')')).match(ARGUMENT_NAMES);
-    if(result === null)
-        result = [];
-    return result;
-}
-
 // Метод, который будет выполнять операции над коллекцией один за другим
 module.exports.query = function (collection /* операторы через запятую */) {
     if (collection.length === 0) {
